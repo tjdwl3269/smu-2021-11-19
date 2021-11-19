fd22d72a51c21bb5b6b21db1def8a57203899f6a
hanjiseong <55674646+tjdwl3269@users.noreply.github.com>
2021-11-19 10:05:32 +0900

    simple

---
+++ simple.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var app = http.createServer(function(request,response){
+    console.log('request');
+    response.end(`
+    <h1><a href="index.html">WEB</a></h1>
+    <ul>
+        <li><a href="html.html">html</a></li>
+        <li><a href="nodejs.html">nodejs</a></li>
+        <li><a href="mysql.html">mysql</a></li>
+    </ul>
+    <h2>Welcome</h2>
+    Hello WEB!!    
+    `);
+});
+app.listen(8080);
+
+// pm2 start simple.js --watch --no-daemon
